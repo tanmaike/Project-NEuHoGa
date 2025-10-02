ce5f5b683087749cbdc8c2577cd8956591719e25
tanmaike <materrazas076@gmail.com>
2025-10-02 13:58:00 -0700

    Added player and movement scripts (w/jumping)

---
+++ Assets/Entities/Player/Scripts/MoveCamera.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+    public Transform cameraPosition;
+
+    public void Update() {
+        transform.position = cameraPosition.position;
+    }
+}
