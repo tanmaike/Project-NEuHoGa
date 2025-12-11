2db040c1bc305802b2f8c0289f5da808d9b08a2e
tan <71731819+tanmaike@users.noreply.github.com>
2025-12-10 18:18:10 -0800

    Notes can be picked up and read

--- Assets/Inventory Items/Scripts/NoteManager.cs
+++ Assets/Inventory Items/Scripts/NoteManager.cs
@@ -9,10 +9,16 @@ public class NoteManager : MonoBehaviour
     [Header("Notes")]
     public GameObject plateNoteButton;
     public GameObject couchNoteButton;
+    public GameObject firstKitchenNoteButton;
+    public GameObject bathroomNoteButton;
+    public GameObject bedroomNoteButton;
 
     [Header("Note Texts")]
     public GameObject plateNoteText;
     public GameObject couchNoteText;
+    public GameObject firstKitchenNoteText;
+    public GameObject bathroomNoteText;
+    public GameObject bedroomNoteText;
 
     private void Awake()
     {
@@ -20,9 +26,15 @@ public class NoteManager : MonoBehaviour
 
         plateNoteButton.SetActive(false);
         couchNoteButton.SetActive(false);
+        firstKitchenNoteButton.SetActive(false);
+        bathroomNoteButton.SetActive(false);
+        bedroomNoteButton.SetActive(false);
 
         plateNoteText.SetActive(false);
         couchNoteText.SetActive(false);
+        firstKitchenNoteText.SetActive(false);
+        bathroomNoteText.SetActive(false);
+        bedroomNoteText.SetActive(false);
     }
 
     public void UnlockNote(string noteID)
@@ -32,9 +44,17 @@ public class NoteManager : MonoBehaviour
             case "001":
                 plateNoteButton.SetActive(true);
                 break;
-
             case "002":
                 couchNoteButton.SetActive(true);
+                break;
+            case "003":
+                firstKitchenNoteButton.SetActive(true);
+                break;
+            case "004":
+                bathroomNoteButton.SetActive(true);
+                break;
+            case "005":
+                bedroomNoteButton.SetActive(true);
                 break;
         }
     }
@@ -43,15 +63,26 @@ public class NoteManager : MonoBehaviour
     {
         plateNoteText.SetActive(false);
         couchNoteText.SetActive(false);
+        firstKitchenNoteText.SetActive(false);
+        bathroomNoteText.SetActive(false);
+        bedroomNoteText.SetActive(false);
 
         switch (noteID)
         {
-            case "PlateNote":
+            case "001":
                 plateNoteText.SetActive(true);
                 break;
-
-            case "CouchNote":
+            case "002":
                 couchNoteText.SetActive(true);
+                break;
+            case "003":
+                firstKitchenNoteText.SetActive(true);
+                break;
+            case "004":
+                bathroomNoteText.SetActive(true);
+                break;
+            case "005":
+                bedroomNoteText.SetActive(true);
                 break;
         }
     }
@@ -60,5 +91,8 @@ public class NoteManager : MonoBehaviour
     {
         plateNoteText.SetActive(false);
         couchNoteText.SetActive(false);
+        firstKitchenNoteText.SetActive(false);
+        bathroomNoteText.SetActive(false);
+        bedroomNoteText.SetActive(false);
     }
 }
