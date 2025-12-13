ecd8215b4b9dfb939a1c3b284d2373f59cc80b6e
Heishnoiz <heish.noiz@gmail.com>
2025-12-12 16:52:27 -0800

    added credits section

--- Assets/Scenes/MenuScene/Scripts/MainMenu.cs
+++ Assets/Scenes/MenuScene/Scripts/MainMenu.cs
@@ -5,6 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject playbutton;
+    public GameObject CreditButton;
+    public GameObject quitButton;
+    public GameObject backButton;
+    public GameObject Credits;
+
+    public void Awake()
+    {
+        playbutton.SetActive(true);
+        CreditButton.SetActive(true);
+        quitButton.SetActive(true);
+        backButton.SetActive(false);
+        Credits.SetActive(false);
+    }
+    
+    public void Back()
+    {
+        playbutton.SetActive(true);
+        CreditButton.SetActive(true);
+        quitButton.SetActive(true);
+        backButton.SetActive(false);
+        Credits.SetActive(false);
+    }
+    public void ShowCredits()
+    {
+        playbutton.SetActive(false);
+        CreditButton.SetActive(false);
+        quitButton.SetActive(false);
+        backButton.SetActive(true);
+        Credits.SetActive(true);
+    }
     public void playGame(){
         SceneManager.LoadScene("GameScene");
     }
@@ -17,4 +48,6 @@ public class MainMenu : MonoBehaviour
             Application.Quit();
         #endif
     }
+
+    
 }
