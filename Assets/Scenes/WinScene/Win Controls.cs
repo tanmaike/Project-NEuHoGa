462e854dbaf4694f1bb0518b057f85d0cca484a1
Heishnoiz <heish.noiz@gmail.com>
2025-12-14 10:55:40 -0800

    Finished win screen

---
+++ Assets/Scenes/WinScene/Win Controls.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinControls : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+     }
+}
